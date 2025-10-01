c4e50d9da7127eca113a664cf40fcf241bb06b3c
Prakash Vardhan <118066597+vardhan967@users.noreply.github.com>
2025-10-01 13:46:10 +0530

    Update settings.py

--- two_factor_auth/settings.py
+++ two_factor_auth/settings.py
@@ -39,7 +39,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'satyaprakashvardhan@gmail.com'  # your Gmail
+EMAIL_HOST_USER = 'Your@gmail.com'  # your Gmail
 EMAIL_HOST_PASSWORD = 'qsrd brfi uezh qdbf'       # your Gmail app password
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
